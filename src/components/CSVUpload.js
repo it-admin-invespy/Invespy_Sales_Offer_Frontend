c7b35227f0ae69e4944fb1ca2672962865aebae2
Sameer-472 <muhammadsameer2016@yahoo.com>
2026-04-15 15:35:15 +0500

    fixed originalPrice issue

--- src/components/CSVUpload.js
+++ src/components/CSVUpload.js
@@ -80,8 +80,7 @@ export default function CSVUpload({
                 ? parseFloat(values[4]?.trim()) || 0
                 : parseFloat(values[5]?.trim()) || 0,
               originalPrice: hasMergedFloorUnit
-                ? parseFloat(values[5]?.replace(/\D/g, "").trim()) || 0
-                : parseFloat(values[6]?.replace(/\D/g, "").trim()) || 0,
+                 && parseFloat(values[5]?.replace(/\D/g, "").trim()) || 0 ,
               price: hasMergedFloorUnit
                 ? parseFloat(values[6]?.replace(/\D/g, "").trim()) || 0
                 : parseFloat(values[6]?.replace(/\D/g, "").trim()) || 0,
