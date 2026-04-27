a1981098e33ed9fda89cfc775b9b6663397edea9
Sameer-472 <muhammadsameer2016@yahoo.com>
2026-04-27 16:49:38 +0500

    change logo width and height

--- src/components/VAT-Form/PDFPage.js
+++ src/components/VAT-Form/PDFPage.js
@@ -188,10 +188,10 @@ const PDFPage = ({ salesOfferData, selectedUnit }) => {
           textAlign: "center",
         }}
       >
-        <p style={{ margin: "3px 0", fontWeight: "bold", fontSize: "12px" }} className="hidden">
+        <p style={{ margin: "3px 0", fontWeight: "bold", fontSize: "12px" }} className="">
           {customer.email}
         </p>
-        <p style={{ margin: "3px 0", fontSize: "12px" }} className="hidden">{customer.address}</p>
+        <p style={{ margin: "3px 0", fontSize: "12px" }} className="">{customer.address}</p>
       </div>
     </div>
   );
